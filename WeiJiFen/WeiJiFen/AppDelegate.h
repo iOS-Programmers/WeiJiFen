be1500a45a615b938195cb510ecb7d84d458ac3d
Johnny <iyhjiang@gmail.com>
2014-10-14 14:12:17 +0800

    first init  初始化项目

---
+++ WeiJiFen/WeiJiFen/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WeiJiFen
+//
+//  Created by Chemayi on 14/10/14.
+//  Copyright (c) 2014年 YH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
